ccfa711d512c0a4b2d943e97e3ce7bc743a37c87
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-16 09:18:00 -0600

    Build test for add cars

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -1,4 +1,5 @@
 require './lib/train'
+require './lib/car'
 require 'rspec'
 
 describe Train do
@@ -14,5 +15,23 @@ describe Train do
     expect(train1.name).to eq('Thomas')
     expect(train1.type).to eq('Tank')
     expect(train1.cargo).to eq({})
+  end
+
+  it '.add_cars' do
+    train1 = Train.new({name: 'Thomas', type: 'Tank'})
+    car1 = Car.new({type: 'Mail', weight: 5})
+
+    train1.add_cars(car1, 2)
+
+    expect(train1.cargo).to eq(car1 => 2)
+
+    train1.add_cars(car1, 3)
+
+    expect(train1.cargo).to eq(car1 => 5)
+  end
+
+  xit '.count_cars' do
+
+    expect(train1.count_cars(car1)).to eq(0)
   end
 end
