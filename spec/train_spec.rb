5b915632c42f8d39cbc442856ecc15525a954836
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-16 09:22:01 -0600

    Build test for count cars

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -20,18 +20,33 @@ describe Train do
   it '.add_cars' do
     train1 = Train.new({name: 'Thomas', type: 'Tank'})
     car1 = Car.new({type: 'Mail', weight: 5})
+    car2 = Car.new({type: 'Passengers', weight: 1})
 
     train1.add_cars(car1, 2)
 
-    expect(train1.cargo).to eq(car1 => 2)
+    expect(train1.cargo).to eq({car1 => 2})
 
     train1.add_cars(car1, 3)
 
-    expect(train1.cargo).to eq(car1 => 5)
+    expect(train1.cargo).to eq({car1 => 5})
+
+    train1.add_cars(car2, 10)
+
+    expect(train1.cargo).to eq({car1 => 5, car2 => 10})
   end
 
-  xit '.count_cars' do
+  it '.count_cars' do
+    train1 = Train.new({name: 'Thomas', type: 'Tank'})
+    car1 = Car.new({type: 'Mail', weight: 5})
 
     expect(train1.count_cars(car1)).to eq(0)
+
+    train1.add_cars(car1, 2)
+
+    expect(train1.count_cars(car1)).to eq(2)
+
+    train1.add_cars(car1, 3)
+
+    expect(train1.count_cars(car1)).to eq(5)
   end
 end
