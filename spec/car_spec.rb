7cdc5c25bec85fdb6536fa24c121df2da89b9335
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-16 09:06:18 -0600

    Build test for exists and attributes

---
+++ spec/car_spec.rb
@@ -0,0 +1,17 @@
+require './lib/car'
+require 'rspec'
+
+describe Car do
+  it 'exists' do
+    car1 = Car.new({type: 'Mail', weight: 5})
+
+    expect(car1).to be_a(Car)
+  end
+
+  it 'attributes' do
+    car1 = Car.new({type: 'Mail', weight: 5})
+
+    expect(car1.type).to eq('Mail')
+    expect(car1.weight).to eq(5)
+  end
+end
